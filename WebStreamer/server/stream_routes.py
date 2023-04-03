0d2a3514ae12e8d178eca7bdee26af6fc67d7ac4
gitdrive5 <121691578+gitdrive5@users.noreply.github.com>
2023-04-04 02:01:40 +0530

    Update stream_routes.py

--- WebStreamer/server/stream_routes.py
+++ WebStreamer/server/stream_routes.py
@@ -9,18 +9,7 @@ from aiohttp import web
 from ..bot import StreamBot
 from ..utils.custom_dl import TGCustomYield, chunk_size, offset_fix
 
-from time import sleep, time
-from psutil import boot_time, disk_usage, net_io_counters
-from subprocess import check_output
-from os import path as ospath
-botStartTime = time()
-if ospath.exists('.git'):
-    commit_date = check_output(["git log -1 --date=format:'%y/%m/%d %H:%M' --pretty=format:'%cd'"], shell=True).decode()
-else:
-    commit_date = 'No UPSTREAM_REPO'
-
 routes = web.RouteTableDef()
-
 
 @routes.get("/", allow_head=True)
 async def root_route_handler(request):
@@ -29,24 +18,6 @@ async def root_route_handler(request):
                               "maintained_by": "Avishkar_Patil",
                               "server_permission": "Open",
                               "Telegram_Bot": '@'+bot_details.username})
-
-@app.route('/status', methods=['GET'])
-def status():
-    bot_uptime = time() - botStartTime
-    uptime = time() - boot_time()
-    sent = net_io_counters().bytes_sent
-    recv = net_io_counters().bytes_recv
-    return {
-        'commit_date': commit_date,
-        'uptime': uptime,
-        'on_time': bot_uptime,
-        'free_disk': disk_usage('.').free,
-        'total_disk': disk_usage('.').total,
-        'network': {
-            'sent': sent,
-            'recv': recv,
-        },
-    }
 
 @routes.get("/{message_id}")
 @routes.get("/{message_id}/")
