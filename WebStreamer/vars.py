038ba8e0edb4ef20c5f1c144bbe2bdde7ccb4631
NOBUDDY PHOTO <123955383+nobuddyphoto1@users.noreply.github.com>
2023-06-30 21:18:31 +0530

    Update vars.py

--- WebStreamer/vars.py
+++ WebStreamer/vars.py
@@ -7,27 +7,27 @@ load_dotenv()
 
 
 class Var(object):
-    API_ID = int(os.environ.get('API_ID'))
-    API_HASH = str(os.environ.get('API_HASH'))
-    BOT_TOKEN = str(os.environ.get('BOT_TOKEN'))
-    SESSION_NAME = str(os.environ.get('SESSION_NAME'))
-    SLEEP_THRESHOLD = int(os.environ.get('SLEEP_THRESHOLD', '60'))
-    WORKERS = int(os.environ.get('WORKERS', '4'))
-    BIN_CHANNEL = int(os.environ.get('BIN_CHANNEL'))
-    PORT = int(os.environ.get('PORT', 8080))
-    BIND_ADRESS = str(os.environ.get('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
-    OWNER_ID = int(os.environ.get('OWNER_ID'))
-    NO_PORT = bool(os.environ.get('NO_PORT', False))
+    API_ID = int(os.environ.get("API_ID"))
+    API_HASH = os.environ.get("API_HASH")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN")
+    SESSION_NAME = os.environ.get("SESSION_NAME")
+    SLEEP_THRESHOLD = int(os.environ.get("SLEEP_THRESHOLD", "60"))
+    WORKERS = int(os.environ.get("WORKERS", "4"))
+    BIN_CHANNEL = int(os.environ.get("BIN_CHANNEL"))
+    PORT = int(os.environ.get("PORT", "8080"))
+    BIND_ADRESS = str(os.environ.get("WEB_SERVER_BIND_ADDRESS", "0.0.0.0"))
+    OWNER_ID = int(os.environ.get("OWNER_ID"))
+    NO_PORT = bool(os.environ.get("NO_PORT", False))
     APP_NAME = None
     if 'DYNO' in environ:
         ON_HEROKU = True
-        APP_NAME = str(os.environ.get('APP_NAME'))
+        APP_NAME = os.environ.get("APP_NAME")
     else:
         ON_HEROKU = False
-    FQDN = str(os.environ.get('FQDN', BIND_ADRESS)) if not ON_HEROKU or os.environ.get('FQDN') else APP_NAME+'.herokuapp.com'
+    FQDN = os.environ.get("FQDN", BIND_ADRESS) if not ON_HEROKU or os.environ.get("FQDN") else APP_NAME+'.adaptable.app'
     URL = "https://{}/".format(FQDN) if ON_HEROKU or NO_PORT else \
         "http://{}:{}/".format(FQDN, PORT)
-    DATABASE_URL = str(os.environ.get('DATABASE_URL'))
-    PING_INTERVAL = int(os.environ.get('PING_INTERVAL', '500'))
-    UPDATES_CHANNEL = str(os.environ.get('UPDATES_CHANNEL', None))
+    DATABASE_URL = os.environ.get("DATABASE_URL")
+    PING_INTERVAL = int(os.environ.get("PING_INTERVAL", "200"))
+    UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", None)
     BANNED_CHANNELS = list(set(int(x) for x in str(os.environ.get("BANNED_CHANNELS", "-1001236895100")).split()))
